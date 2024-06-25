0129244482e3ec1094b4103a60de68488a669bcf
Thiago Gonçalves <mg.az.ul@hotmail.com>
2024-06-25 16:38:46 -0300

    User refactor, fix circular serialization

--- social-media/src/main/java/dev/tsantana/social_media/domain/User.java
+++ social-media/src/main/java/dev/tsantana/social_media/domain/User.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -35,18 +37,23 @@ public class User {
 	private String gender;
 	private String photoUri;
 
+	@JsonIgnore
 	@OneToMany(cascade = CascadeType.ALL)
 	private Set<User> followers = new HashSet<User>();
 
+	@JsonIgnore
 	@OneToMany(cascade = CascadeType.ALL)
 	private Set<User> followed = new HashSet<User>();
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	private Set<Post> posts = new HashSet<Post>();
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	private Set<Album> albums = new HashSet<Album>();
-	
+
+	@JsonIgnore
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	private List<Photo> photos = new ArrayList<Photo>();
 
@@ -120,16 +127,15 @@ public class User {
 	public void addPhotos(Photo photo) {
 		this.photos.add(photo);
 	}
-	
+
 	public void removePhotos(Photo photo) {
 		this.photos.remove(photo);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + ", birth=" + birth + ", gender=" + gender
 				+ ", photoUri=" + photoUri + ", posts=" + posts + "]";
 	}
 
-	
 }
