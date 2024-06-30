92a60cf6128eec9501ae27f6463ba938d0908534
Thiago Gonçalves <mg.az.ul@hotmail.com>
2024-06-30 12:27:25 -0300

    UserService refactor, findAllPaged added

--- social-media/src/main/java/dev/tsantana/social_media/service/UserService.java
+++ social-media/src/main/java/dev/tsantana/social_media/service/UserService.java
@@ -2,6 +2,8 @@ package dev.tsantana.social_media.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import dev.tsantana.social_media.domain.User;
@@ -12,6 +14,8 @@ public interface UserService {
 	User insert(User user);
 
 	List<User> findAll();
+	
+	Page<User> findAllPaged(Pageable pageable);
 
 	User findById(Long userId);
 
